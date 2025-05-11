b6ad2e42c282b2993fcbc23cde4d04f275d4524d
Martin Villa Arreola <78709520+VillaArreola@users.noreply.github.com>
2025-05-10 20:18:36 -0400

    Pagina index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,17 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md}"],
   theme: {
     extend: {
-      colors: {
-        terminalBg: "#0d1117",
-        terminalGreen: "#39ff14",
-        terminalCyan: "#00ffff",
-      },
-      fontFamily: {
-        mono: ['"Fira Code"', 'monospace'],
-      },
-    },
+  colors: {
+    terminalBg: '#0d1117',        // fondo casi negro
+    terminalGreen: '#39ff14',     // verde fosforescente
+    terminalCyan: '#00ffff',      // cyan brillante
+    terminalGray: '#1f2937',      // gris oscuro para bordes/cajas
+  },
+  fontFamily: {
+    mono: ['"Fira Code"', 'monospace'],
+  },
+}
+
   },
   plugins: [require('@tailwindcss/typography')],
 };
